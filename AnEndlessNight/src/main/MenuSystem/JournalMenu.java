6f37dd706222196960215bf633b90925770ce4a3
CalebCrosbySears <csears@ggc.edu>
2016-11-02 17:59:57 -0400

    JournalMenue

---
+++ AnEndlessNight/src/main/MenuSystem/JournalMenu.java
@@ -0,0 +1,22 @@
+package main.MenuSystem;
+
+public class JournalMenu extends AbstractMenu{
+
+	public JournalMenu(AbstractMenu lastMenu) {
+		super(lastMenu);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void mainPrompt() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void onDestroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
