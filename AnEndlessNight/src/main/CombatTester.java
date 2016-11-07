7b3156a5ec4decd2bb4e7a93cd4d9dd7b62a38d7
Aaron <aknobloch@ggc.edu>
2016-11-07 10:55:11 -0500

    Finishes CombatMenu and adds a tester.

---
+++ AnEndlessNight/src/main/CombatTester.java
@@ -0,0 +1,14 @@
+package main;
+
+import main.MenuSystem.MenuLoader;
+
+public class CombatTester {
+
+	public static void main(String[] args) {
+		
+		Game.initializeGame();
+		MenuLoader.loadStartMenu(null);
+		
+	}
+	
+}
--- AnEndlessNight/src/main/MenuSystem/CombatMenu.java
+++ AnEndlessNight/src/main/MenuSystem/CombatMenu.java
@@ -6,9 +6,16 @@ import main.Game;
 import main.CombatSystem.DamageHandler;
 import main.CombatSystem.EscapeController;
 import main.CombatSystem.Monster;
+import main.CombatSystem.StatusCondition;
 
+/**
+ * 
+ * @author Aaron
+ *
+ */
 public class CombatMenu extends AbstractMenu {
 
+	static boolean battleContinuing;
 
 	public CombatMenu(MenuLoader menuLoader) {
 		super(menuLoader);
@@ -19,68 +26,109 @@ public class CombatMenu extends AbstractMenu {
 		
 		Monster currentMonster = Game.getHero().getRoom().getMonster();
 		System.out.println("A " + currentMonster.getName() + " stares you down.");
-		System.out.println("What is your next move?");
-		System.out.println();
-		System.out.println("1. Attack Monster");
-		System.out.println("2. Brace Yourself");
-		System.out.println("3. Attempt to Flee");
-		System.out.println("4. Use Item");
-		System.out.println();
+		battleContinuing = true;
 		
 		
-		int userChoice = -1;
-		boolean validInput = false;
-		
-		do {
-			try {
-				
-				userChoice = GameInput.getInt();
-				validInput = true;
-				
-			} catch (IOException e) {
-				
-				System.out.println("You mumble incoherently. You should be wary of nonsensical babbling, ");
-				System.out.println("else you be labeled insane.");
-				System.out.println();
-				validInput = false;
-				
+		while(battleContinuing) {
+			
+			System.out.println("What is your next move?");
+			System.out.println();
+			System.out.println("1. Attack Monster");
+			System.out.println("2. Brace Yourself");
+			System.out.println("3. Attempt to Flee");
+			System.out.println("4. Use Item");
+			System.out.println();
+
+
+			int userChoice = -1;
+			boolean validInput = false;
+
+			do {
+				try {
+
+					userChoice = GameInput.getInt();
+					validInput = true;
+
+				} catch (IOException e) {
+
+					System.out.println("You mumble incoherently. You should be wary of nonsensical babbling, ");
+					System.out.println("else you be labeled insane.");
+					System.out.println();
+					validInput = false;
+
+				}
+
+			} while( ! validInput);
+
+			if(userChoice == 1) {
+
+				attack(currentMonster);
+
 			}
-			
-		} while( ! validInput);
+			else if(userChoice == 2) {
+
+				defend(currentMonster);
+
+			}
+			else if(userChoice == 3) {
+
+				flee(currentMonster);
+
+			}
+			else if(userChoice == 4) {
+				// TODO: handle item use
+			}
+			else {
+				// something went wrong
+				this.mainPrompt();
+			}
+		}
 		
-		if(userChoice == 1) {
-			
-			attack(currentMonster);
-			
+	}
+
+	private void attack(Monster currentMonster) {
+		
+		DamageHandler combatManager = new DamageHandler();
+		System.out.println("You raise your " + Game.getHero().getEquippedWeapon().getName() + 
+						   " and bring it down on the " + currentMonster.getName());
+		
+		int monsterAttackResult = combatManager.attackMonster();
+		
+		if(monsterAttackResult == 0) 
+		{
+			System.out.println("Your attack barely grazes the creature, leaving it undamaged.");
+			System.out.println();
+			monsterAttack();
 		}
-		else if(userChoice == 2) {
+		else if(monsterAttackResult == -1) 
+		{
+			System.out.println("Your attack sinks deep into the " + currentMonster.getName() + 
+							   ", mortally wounding it.");
+			System.out.println("Its body slumps over as the last breath escapes its body.");
+			System.out.println();
 			
-			defend(currentMonster);
-			
+			battleContinuing = false;
+			MenuLoader.loadGameMenu(this);
 		}
-		else if(userChoice == 3) {
-			
-			flee(currentMonster);
-			
-		}
-		else if(userChoice == 4) {
-			// TODO: handle item use
-		}
-		else {
-			// something went wrong
-			this.mainPrompt();
+		else 
+		{
+			System.out.println("Your weapon vibrates as it strikes the creature, wounding it.");
+			System.out.println("The creature has " + monsterAttackResult + " health remaining.");
+			System.out.println();
+			monsterAttack();
 		}
 		
 	}
 
 	private void defend(Monster currentMonster) {
 		
-		DamageHandler combatManager = new DamageHandler();
-		int monsterDamage = currentMonster.getStrength();
-		// defending reduces monster damage by 1/3
-		monsterDamage = monsterDamage - (monsterDamage / 3);
+		Game.getHero().addStatusCondition(StatusCondition.DEFENSE_BUFF);
 		
 		System.out.println("You brace yourself for an attack.");
+		
+		monsterAttack();
+		
+		Game.getHero().removeStatusCondition(StatusCondition.DEFENSE_BUFF);
 		
 	}
 
@@ -93,6 +141,7 @@ public class CombatMenu extends AbstractMenu {
 		
 			System.out.println("You feign an attack, and dart off while the " + battleMonster.getName() + " attempts to brace itself.");
 			System.out.println();
+			battleContinuing = false;
 			MenuLoader.loadGameMenu(this);
 			
 		}
@@ -100,34 +149,44 @@ public class CombatMenu extends AbstractMenu {
 			
 			System.out.println("You try to escape, but the " + battleMonster.getName() + " is too fast.");
 			
-			DamageHandler combat = new DamageHandler();
-			int combatResult = combat.attackHero();
-			
-			if(combatResult == -1) {
-				
-				heroDeath();
-				
-			}
-			else if(combatResult == 0) {
-				
-				System.out.println("The creature attempts to attack, but you swiftly dodge.");
-				System.out.println();
-				
-			}
-			else {
-				
-				System.out.println("It attacks you, leaving you with " + combatResult + " left.");
-				System.out.println();
-				
-			}
+			monsterAttack();
 			
 		}
+	}
+
+	private void monsterAttack() {
+		
+		System.out.println("The creature attacks!");
+		
+		DamageHandler combat = new DamageHandler();
+		int combatResult = combat.attackHero();
+		
+		if(combatResult == -1) {
+			
+			battleContinuing = false;
+			heroDeath();
+			
+		}
+		else if(combatResult == 0) {
+			
+			System.out.println("The creature lunges, but you swiftly dodge.");
+			System.out.println();
+			
+		}
+		else {
+			
+			System.out.println("It attacks you, leaving you with " + combatResult + " health left.");
+			System.out.println();
+			
+		}
+		
 	}
 
 	private void heroDeath() {
 		
 		System.out.println("Your knees crumple and your vision fades as your wounds become too much to bear.");
 		System.out.println();
+		battleContinuing = false;
 		MenuLoader.loadStartMenu(this);
 		
 	}
